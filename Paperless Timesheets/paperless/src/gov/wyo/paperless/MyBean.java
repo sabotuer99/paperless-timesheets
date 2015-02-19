0176cd4aa31082f1fd91a7ec2433456a07981244
troy <sabotuer99@gmail.com>
2015-02-18 22:42:23 -0700

    Missed some files in commit

---
+++ Paperless Timesheets/paperless/src/gov/wyo/paperless/MyBean.java
@@ -0,0 +1,15 @@
+package gov.wyo.paperless;
+
+/** The object model for the data we are sending through endpoints */
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
