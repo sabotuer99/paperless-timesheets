f16d917d41b101e2f16d0f81de73445e96168da4
troy <sabotuer99@gmail.com>
2015-02-21 22:21:31 -0700

    testing git ignore

--- Paperless Timesheets/src/gov/wyo/paperless/Constants.java
+++ Paperless Timesheets/src/gov/wyo/paperless/Constants.java
@@ -5,7 +5,7 @@ package gov.wyo.paperless;
  */
 public class Constants {
   public static final String APPLICATION_NAME = "paperless-timesheets";
-  public static final String WEB_CLIENT_ID = "";
+  public static final String WEB_CLIENT_ID = "blah blah balh";
   public static final String SIMPLE_API_ACCESS_KEY = "";
   public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
   public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
